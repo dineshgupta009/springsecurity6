3e27c82ba348395dcf2c9bb18ad56360ab47d7ff
dineshg009 <dineshg009@gmail.com>
2024-03-12 16:43:39 +0530

    initial commit

--- section1/springsecuritybasic/src/main/java/com/springsecurity6/SpringsecuritybasicApplication.java
+++ section1/springsecuritybasic/src/main/java/com/springsecurity6/SpringsecuritybasicApplication.java
@@ -1,11 +1,9 @@
-package com.eazybytes.springsecuritybasic;
+package com.springsecurity6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.eazybytes.springsecuritybasic.controller") // Optional
 public class SpringsecuritybasicApplication {
 
 	public static void main(String[] args) {
--- section1/springsecuritybasic/src/test/java/com/springsecurity6/SpringsecuritybasicApplicationTests.java
+++ section1/springsecuritybasic/src/test/java/com/springsecurity6/SpringsecuritybasicApplicationTests.java
@@ -1,4 +1,4 @@
-package com.eazybytes.springsecuritybasic;
+package com.springsecurity6;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
